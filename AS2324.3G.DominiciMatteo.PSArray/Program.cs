b8d910e81b553feb6d5689c3e7a5740d128b77c3
Matteo Dominici <dominicimatteo07@gmail.com>
2024-02-29 08:41:01 +0100

    feat: primo punto=funzione StampaVotiPesi che stampa i voti e i loro corrispondenti pesi

--- AS2324.3G.DominiciMatteo.PSArray/Program.cs
+++ AS2324.3G.DominiciMatteo.PSArray/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world");
+            Console.WriteLine("Matteo Dominici; 3G; 29/02/2024; Proca singola Array");
+            Console.WriteLine("Inserisci il numero di voti");
+            int nVoti = int.Parse(Console.ReadLine());
+            double[] voti = new double[nVoti];
+            int[] pesi = new int[nVoti];
+            StampaVotiPesi(voti, pesi, nVoti);
+        }
+        static void StampaVotiPesi(double[] voti, int[] pesi, int nVoti)
+        {
+            Console.WriteLine("Voti, pesi");
+            for (int i = 0; i < nVoti; i++)
+            {
+                Console.WriteLine($"{voti[i]}, {pesi[i]}");
+            }
         }
     }
 }
